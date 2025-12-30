58675da63723f28f27a32901859d84956e4b82ce
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-31 02:26:06 +0530

    List component renamed and other component updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import EmployeeList from "./components/employee/employeeList";
 import Footer from "./components/Footer";
 // import Layout from "./components/layout/Layout";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    
+
     <div className="min-h-screen w-full flex flex-col">
       <Navbar />
-      <div className="flex-1"></div>
+      <div className="flex-1">
+        <EmployeeList />
+      </div>
       <Footer />
     </div>
   );
