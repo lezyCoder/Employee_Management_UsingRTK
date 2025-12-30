dbb77a9c8b6b2d2325425b67a6a9802ae01c3d2c
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-31 02:47:47 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Layout from "./layout/Layout";
+import { FaPlus } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -9,21 +10,8 @@ const Navbar = () => {
             <a className="btn btn-ghost text-xl">Employee</a>
           </div>
           <div className="navbar-end">
-            <button className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />{" "}
-              </svg>
+            <button className="btn btn-ghost btn-circle border">
+              <FaPlus />
             </button>
             <button className="btn btn-ghost btn-circle">
               <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
