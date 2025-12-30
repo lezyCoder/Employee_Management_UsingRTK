1a50397500c8e78522026a814304f73b10a4f9f0
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-31 04:04:02 +0530

    Redux Tool Kit added

--- src/components/employee/EmployeeList.jsx
+++ src/components/employee/EmployeeList.jsx
@@ -1,4 +1,6 @@
 import Layout from "../layout/Layout"
+import { MdModeEdit } from "react-icons/md";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const EmployeeList = () => {
   return (
@@ -26,7 +28,10 @@ const EmployeeCard = () => {
         "Remaining Reason" became an instant hit, praised for its haunting sound and emotional depth. A viral performance brought it widespread recognition, making it one of Dio Lupa’s most iconic tracks.
       </p>
       <button className="btn btn-square btn-ghost">
-        <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
+       <MdModeEdit className="text-xl"/>
+      </button>
+      <button className="btn btn-square btn-ghost">
+       <AiOutlineDelete className="text-xl"/>
       </button>
       <button className="btn btn-square btn-ghost">
         <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import store from "./store/store.js";
+// Import Provider from react-redux instead
+import { Provider } from "react-redux"; 
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
