32e537e3b5727bdf1a9991f2301e2a223d4919e1
lezyCoder <iamvijaysingh23@gmail.com>
2026-01-01 03:49:44 +0530

    EmployeePopUp added to Edit Button

--- src/components/employee/EmployeeList.jsx
+++ src/components/employee/EmployeeList.jsx
@@ -1,8 +1,8 @@
 import Layout from "../layout/Layout"
 import { MdModeEdit } from "react-icons/md";
 import { AiOutlineDelete } from "react-icons/ai";
-import { useDispatch }from "react-redux";
-import { openDeletePopUp } from "../../store/features/popup/popup.slice";
+import { useDispatch } from "react-redux";
+import { openDeletePopUp, openEmployeePopup } from "../../store/features/popup/popup.slice";
 
 const EmployeeList = () => {
   return (
@@ -31,10 +31,10 @@ const EmployeeCard = () => {
         "Remaining Reason" became an instant hit, praised for its haunting sound and emotional depth. A viral performance brought it widespread recognition, making it one of Dio Lupa’s most iconic tracks.
       </p>
       <button className="btn btn-square btn-ghost">
-        <MdModeEdit className="text-xl" />
+        <MdModeEdit className="text-xl" onClick={() => dispatch(openEmployeePopup())} />
       </button>
       <button className="btn btn-square btn-ghost">
-        <AiOutlineDelete className="text-xl"  onClick={()=>dispatch(openDeletePopUp())}/>
+        <AiOutlineDelete className="text-xl" onClick={() => dispatch(openDeletePopUp())} />
       </button>
       <button className="btn btn-square btn-ghost">
         <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
